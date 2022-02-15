751ee0881e7d894043713eacb9323cb2b420a16e
Anar <78043093+PP1-hub@users.noreply.github.com>
2022-02-15 22:28:44 +0600

    js

---
+++ lab 3/JavaScript Info/Objects/static/js/a4.js
@@ -0,0 +1,16 @@
+let calculator = {
+    read() {
+        this.a = +prompt('a?', 0);
+        this.b = +prompt('b?', 0);
+    },
+    mul() {
+        return this.a * this.b;
+    },
+    sum() {
+        return this.a + this.b;
+    }
+};
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
